ccfc19c223bd348c21558c6c73d7e8fdf8a1398a
jakhardefenceacademy-cell <jakhardefenceacademy@gmail.com>
2026-09-21 21:48:52 +0530

    Replace all old file

--- sw.js
+++ sw.js
@@ -1,14 +1,9 @@
-const CACHE='JDA-FINAL-FIX-V1';
-const SHELL=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+const C='JDA-HARD-MOBILE-V2';
+const S=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(S)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  const u=new URL(e.request.url);
-  if(u.origin!==location.origin) return;
-  e.respondWith(caches.match(e.request).then(hit=>hit||fetch(e.request).then(r=>{
-    if(r.ok)caches.open(CACHE).then(c=>c.put(e.request,r.clone()));
-    return r;
-  }).catch(()=>caches.match('./index.html'))));
+ if(e.request.method!=='GET')return;
+ const u=new URL(e.request.url); if(u.origin!==location.origin)return;
+ e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request).then(r=>{if(r.ok)caches.open(C).then(c=>c.put(e.request,r.clone()));return r}).catch(()=>caches.match('./index.html'))));
 });
-self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting()});
