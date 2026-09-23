3947687622a62921076ef5eeaeb2ffb665b2b6eb
jakhardefenceacademy-cell <jakhardefenceacademy@gmail.com>
2026-09-23 19:51:39 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='JDA-V38-GMAIL-PASSWORD-FIXED';
-const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(r.ok)caches.open(CACHE).then(x=>x.put(e.request,r.clone()));return r}).catch(()=>caches.match('./index.html'))))});
